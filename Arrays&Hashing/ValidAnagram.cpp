b83374a612b228e1c4f8b76724aa3451a2b0c07a
PavleProd <pavleprod249@gmail.com>
2023-05-24 10:55:24 +0200

    valid anagram

---
+++ Arrays&Hashing/ValidAnagram.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isAnagram(string& s, string& t) { // O(nlogn) time, O(1) space
+    if(s.size() != t.size()) return false;
+
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+
+    return s == t;
+}
+
+bool isAnagramFaster(string& s, string& t) { // O(1) time, O(1) space
+    if(s.size() != t.size()) return false;
+
+    int frequency[26] = {0};
+
+    for(int i = 0 ; i < s.size(); i++) {
+        frequency[s[i] - 'a']++;
+        frequency[t[i] - 'a']--;
+    }
+
+    return all_of(frequency, frequency + 26, [](int value) -> bool { return value == 0; });
+}
